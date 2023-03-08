a5d51ac45bb034461d59af19fb724d30dd478f2c
nathan <samochval.nathan@gmail.com>
2023-03-09 00:23:39 +0100

    new

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -6,14 +6,79 @@ function darkModeListener() {
 
 document.querySelector("input[type='checkbox']#dark-toggle").addEventListener("click", darkModeListener);
 
-var socket = io()
-var pseudo = "";
-while (pseudo.length <= 0) {
-  pseudo = prompt("Pseudo ?")
-}
-socket.emit('set-pseudo', pseudo);
+// gestion des envois et des receptions
+var socket = io();
+
+// gestion du pseudo
+const nomDialog = document.getElementById('nomDiscord');
+const nomInput = document.getElementById('nom-input');
+const nomSubmit = document.getElementById('nom-submit');
+
+socket.on('request name', () => {
+  // Afficher la boîte de dialogue pour demander le prénom
+  nomDialog.style.display = 'block';
+
+  // Envoyer le prénom de l'utilisateur lorsque le bouton est cliqué
+  nomSubmit.onclick = () => {
+    const nom = nomInput.value;
+    if (nom) {
+      socket.emit('name', nom);
+      nomDialog.style.display = 'none';
+    }
+  };
+});
+
+// gestion des utilisateurs en ligne
+const userList = document.getElementById('utilisateur-list');
+
+socket.on('connected users', (users) => {
+  // Effacer la liste précédente des utilisateurs connectés
+  userList.innerHTML = '';
+
+  // Ajouter chaque utilisateur connecté à la liste
+  const userItems = users.map((user) => {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = user.name;
+    return listItem;
+  });
+  userItems.forEach((item) => {
+    userList.appendChild(item);
+  });
+});
 
 
+// gestion des envoi et des recu de message
 var messages = document.getElementById('messages')
-var form = document.getElementById('form')
 var input = document.getElementById('input')
+var button = document.getElementById('button')
+
+
+button.addEventListener('click', (event) => {
+  event.preventDefault();
+  const message = input.value;
+
+  const messageObject = {
+    type: 'envoi',
+    content: message
+  };
+
+  socket.emit('message', messageObject);
+});
+
+socket.on('message', (data) => {
+  const listItem = document.createElement('li');
+  if (data.type === 'recu') {
+    listItem.classList.add("recu")
+    listItem.innerHTML = data.content;
+  } else if (data.type === 'envoi') {
+    listItem.classList.add("envoi")
+    listItem.innerHTML = data.content;
+  }
+
+  messages.appendChild(listItem);
+});
+
+// gestion de la deconnection
+socket.on('disconnect', () => {
+  console.log('Déconnecté du serveur : ' + socket.id);
+});
--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const {Server} = require("socket.io");
+const { Server } = require("socket.io");
+
 const io = new Server(server);
 var path = require("path");
 const { Socket } = require('dgram');
@@ -11,41 +12,84 @@ let PORT = 3030;
 
 
 server.listen(PORT, () => {
-    console.log("Serveur démarré sur le port :"+PORT);
+  console.log("Serveur démarré sur le port :" + PORT);
 });
 
 
-app.get("/", (req,res) => {
-    res.sendFile(path.join(__dirname, '/views/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/index.html'));
 
 });
 
 // css
-app.get("/style", (req,res) => {
-    res.sendFile(path.join(__dirname, '/public/stylesheets/style.css'));
+app.get("/style", (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/stylesheets/style.css'));
 });
 
-app.get("/fontawesome", (req,res) => {
+app.get("/fontawesome", (req, res) => {
   res.sendFile(path.join(__dirname, '/public/stylesheets/fontawesome-free-6.3.0-web/css/fontawesome.css'));
 });
 
-app.get("/reset.min", (req,res) => {
+app.get("/reset.min", (req, res) => {
   res.sendFile(path.join(__dirname, '/public/stylesheets/reset.min.css'));
 });
 
 // script
-app.get("/script", (req,res) => {
-    res.sendFile(path.join(__dirname, '/public/javascripts/script.js'));
+app.get("/script", (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/javascripts/script.js'));
 });
 
-app.get("/tailwindcss", (req,res) => {
+app.get("/tailwindcss", (req, res) => {
   res.sendFile(path.join(__dirname, '/public/javascripts/tailwindcss.js'));
 });
 
 
-io.on('connection', (socket) =>{
-  socket.on('set-pseudo', (pseudo) =>{
-    console.log(pseudo + " vient de se connecter à "+ new Date());
-    socket.nickname = pseudo
+
+let users = {};
+
+io.on('connection', (socket) => {
+
+  console.log('User connected: ' + socket.id);
+
+  // Demander le prénom de l'utilisateur
+  socket.emit('request name');
+
+  socket.on('name', (name) => {
+    // Stocker l'ID et le prénom de l'utilisateur
+    users[socket.id] = { id: socket.id, name: name };
+
+    // Envoyer la liste des utilisateurs connectés à tous les clients
+    io.emit('connected users', Object.values(users));
+  });
+
+  socket.on('message', (data) => {
+    console.log(`Message reçu : ${data}`);
+
+    const message = {
+      type: 'recu',
+      content: data.content
+    };
+
+    // Envoyer le message reçu à tous les autres clients connectés
+    socket.broadcast.emit('message', message);
+
+    // Envoyer le message au client qui l'a envoyé
+    const response = {
+      type: 'envoi',
+      content: data.content
+    };
+    socket.emit('message', response);
+  });
+
+  // Gérer la perte de connexion du client
+  socket.on('disconnect', () => {
+    console.log('utilisateur deconnecte: ' + socket.id);
+
+    // Retirer l'utilisateur déconnecté de la liste
+    delete users[socket.id];
+
+    // Envoyer la liste des utilisateurs connectés à tous les clients
+    io.emit('connected users', Object.values(users));
+
   });
 });
