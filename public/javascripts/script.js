d2fcc9c4baa70c87146e720bd25c5496dbe154c6
ns <samochval.nathan@gmail.com>
2023-03-13 00:07:19 +0100

    new

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,96 +1,231 @@
 // fonction pour le darkMode
-function darkModeListener() {
-  document.querySelector("html").classList.toggle("dark");
+const root = document.querySelector(':root');
+document.querySelector("input[type='checkbox']#dark-toggle").addEventListener('click', function () {
+  root.classList.toggle('dark-mode');
+});
+
+function inputChat() {
+  document.getElementById('input').addEventListener('keypress', e => {
+    if (e.key === "Enter") {
+      e.preventDefault()
+      mes()
+    }
+  })
 }
 
-document.querySelector("input[type='checkbox']#dark-toggle").addEventListener("click", darkModeListener);
 
-
-
-// gestion des envois et des receptions
+// gestion des envois et des receptions entre le serveurs et le client
 var socket = io();
 
+var pseudoActuel = ''
+var chatSelect = 'General';
 
-// parent de la page login et sign In
+
+
+/*
+
+    Log IN
+
+*/
+
+// valeur des input demander (mail, password)
+var loginEmail = document.getElementById('email-login');
+var loginPassword = document.getElementById('password-login');
+// button submit pour recup les donnees du login
+var loginSubmit = document.getElementById('button-submit-login');
+// message pour dire que les champs sont mal remplit dans les champ login
+var loginErreur = document.getElementById('erreur-login')
+
+
+/*
+
+    Sign IN
+
+*/
+
+// valeur des input demander (pseudo, mail, password)
+var signNom = document.getElementById('name-sign');
+var signEmail = document.getElementById('email-sign');
+var signPassword = document.getElementById('password-sign');
+// button submit pour recup les donnees du sign
+var signSubmit = document.getElementById('button-submit-sign');
+// message pour dire que les champs sont mal remplit dans les champ signIn
+var signErreur = document.getElementById('erreur-sign')
+
+
+/*
+
+    Enregistrement de html avent la supression
+
+*/
+
+// parent html de l'application
+var parentAll = document.getElementById('parentAll')
+
+
+// parent html de la page login, sign In et l'email verif
 var parentLogSign = document.getElementById('loginSign')
+// Stockez l'html dans une variable
+var parentLogSignSauvegarde = parentLogSign.cloneNode(true);
 
-// trouver l'element a supprimer
+
+// contient le html de la page login et signIn
+var logSign = document.getElementById('container')
+// Stockez l'html dans une variable
+var logSignSauvegarde = logSign.cloneNode(true);
+
+
+// contient l'html de toute l'application concernant le chat
 var chat = document.getElementById('discord')
-// Stockez le chat supprimé dans une variable
-const chatSauvegarde = chat.cloneNode(true);
+// Stockez l'html de l'application pour l'utiliser plus tard
+var chatSauvegarde = chat.cloneNode(true);
 // Supprimez l'élément de la page
 chat.remove();
 
 
-// trouver l'element a supprimer
+// contient l'html qui permet de verifier l'email
 var emailVerif = document.getElementById('verif-email')
-// Stockez le chat supprimé dans une variable
-const emailVerifSauvegarde = emailVerif.cloneNode(true);
+// Stockez l'html pour la verification d'email pour l'utiliser plus tard
+var emailVerifSauvegarde = emailVerif.cloneNode(true);
 // Supprimez l'élément de la page
 emailVerif.remove();
 
-var loginEmail = document.getElementById('email-login');
-var loginPassword = document.getElementById('password-login');
-var loginSubmit = document.getElementById('button-submit-login');
 
-var signNom = document.getElementById('name-sign');
-var signEmail = document.getElementById('email-sign');
-var signPassword = document.getElementById('password-sign');
-var signSubmit = document.getElementById('button-submit-sign');
+// variable contenant le information pour l'inscription (pseudo, mail, password)
+let SignObjet;
 
-var signErreur = document.getElementById('erreur-sign')
 
-var logSign = document.getElementById('container')
+/*
 
-loginSubmit.addEventListener('click', (e) => {
-  e.preventDefault();
-  console.log(loginEmail.value)
-  const LoginObjet = {
-    email: loginEmail.value,
-    password: loginPassword.value
-  }
-})
+    gestion d'un nouvelle utilisateur
 
+*/
 
 signSubmit.addEventListener('click', (e) => {
   e.preventDefault();
-  var good = signNom.value.length * signEmail.value.length * signPassword.value.length;
+  let good = signNom.value.length * signEmail.value.length * signPassword.value.length;
   if (good !== 0) {
     if (signPassword.value.length > 8) {
-      const SignObjet = {
+      SignObjet = {
         pseudo: signNom.value,
         email: signEmail.value,
         password: signPassword.value
       }
-      // Stockez l'élément supprimé dans une variable
-      const logSignSauvegarde = logSign.cloneNode(true);
-      // Supprimez l'élément de la page
-      logSign.remove();
-
-      // pour verif l'email on afficher l'element suprimer qui permet de verif l'email
-      parentLogSign.appendChild(emailVerifSauvegarde);
-
       socket.emit('sign', SignObjet);
-    }else{
+      socket.on('verifEmail', (exist) => {
+        if (exist) {
+          // Supprimez l'élément de la page
+          logSign.remove();
+
+          // pour verif l'email on afficher l'element suprimer qui permet de verif l'email
+          parentLogSign.appendChild(emailVerifSauvegarde);
+
+
+          /*
+
+              Email Verification
+
+          */
+          socket.on('verif', code => {
+            // valeur du code de verification
+            var codeEmail = document.getElementById('verif-email-text')
+            //bouton pour valider l'entre
+            var suivant = document.getElementById('next')
+            //bouton pour retourner en arriere
+            var retour = document.getElementById('retour')
+            // message pour dire que le code n'est pas le bon ou que l'adresse mail n'existe pas
+            var erreurVerif = document.getElementById('erreur-verif')
+
+
+            // verification de l'email apres la saisie du code
+            suivant.addEventListener('click', (e) => {
+              e.preventDefault();
+              if (codeEmail.value == code) {
+                pseudoActuel = valide.pseudo
+                // Supprimez l'élément de la page
+                parentLogSign.remove();
+                // ajout de l'application du chat
+                parentAll.appendChild(chatSauvegarde);
+                // envoie des donner vers la base de donne
+                socket.emit('register', SignObjet);
+                socket.emit('syncMessage', chatSelect)
+                inputChat()
+              } else {
+                erreurVerif.innerText = "Incorrect code or email does not exist"
+              }
+            });
+          })
+        } else {
+          signErreur.innerText = "Email already use "
+        }
+      })
+    } else {
       signErreur.innerText = "Password too short "
     }
-  }else{
+  } else {
     signErreur.innerText = "Fields not filled"
   }
 })
 
-// gestion des utilisateurs en ligne
-const userList = document.getElementById('utilisateur-list');
+
+/*
+
+    gestion d'une connexion d'un utilisateur deja existant
+
+*/
+
+loginSubmit.addEventListener('click', (e) => {
+  e.preventDefault();
+  var good = loginEmail.value.length * loginPassword.value.length
+  if (good !== 0) {
+    const LoginObjet = {
+      email: loginEmail.value,
+      password: loginPassword.value
+    }
+    socket.emit('login', LoginObjet)
+    socket.on('login', valide => {
+      if (valide.exists) {
+        pseudoActuel = valide.pseudo
+        // Supprimez l'élément de la page
+        parentLogSign.remove();
+        // ajout de l'application du chat
+        parentAll.appendChild(chatSauvegarde);
+        socket.emit('syncMessage', chatSelect)
+        inputChat()
+      } else {
+        loginErreur.innerHTML = "Incorrect email or password"
+      }
+    })
+  } else {
+    loginErreur.innerHTML = "Fields not filled"
+  }
+})
+
+
+/*
+
+    gestion des utilisateurs en ligne
+
+*/
+
 
 socket.on('connected users', (users) => {
+  // list des utilisateur connecter
+  let userList = document.getElementById('utilisateur-list');
   // Effacer la liste précédente des utilisateurs connectés
   userList.innerHTML = '';
 
   // Ajouter chaque utilisateur connecté à la liste
-  const userItems = users.map((user) => {
-    const listItem = document.createElement('li');
-    listItem.innerHTML = user.name;
-    return listItem;
+  let userItems = users.map((user) => {
+    if (pseudoActuel === user.name) {
+      let listItem = document.createElement('li');
+      listItem.innerHTML = 'You';
+      return listItem;
+    } else if (pseudoActuel !== user.name) {
+      let listItem = document.createElement('li');
+      listItem.innerHTML = `<button onclick="selectionChat('${user.name}')" class="button-user">${user.name}</button>`;
+      return listItem;
+    }
   });
   userItems.forEach((item) => {
     userList.appendChild(item);
@@ -98,34 +233,89 @@ socket.on('connected users', (users) => {
 });
 
 
-// gestion des envoi et des recu de message
-var messages = document.getElementById('messages')
-var input = document.getElementById('input')
-var button = document.getElementById('button')
+
+/*
+
+    gestion des envoi et des recu de message
+
+*/
 
 
-button.addEventListener('click', (event) => {
-  event.preventDefault();
-  const message = input.value;
+/*
 
-  const messageObject = {
-    type: 'envoi',
+    Syncronisation des messages
+
+*/
+socket.on('sync', (Allmessages) => {
+  var messagelist = document.getElementById('messages');
+  messagelist.innerHTML = '';
+  console.log(Allmessages)
+  let messages = Allmessages.map((message) => {
+    if (message.emetteur === pseudoActuel) {
+      if(message.destinataire === chatSelect){
+        let listItem = document.createElement('li');
+        listItem.classList.add("envoi");
+        listItem.innerHTML = `${message.content}`;
+        return listItem;
+      }
+    } else if (message.destinataire === chatSelect) {
+      let listItem = document.createElement('li');
+      listItem.classList.add("recu")
+      listItem.innerHTML = `${message.content}`;
+      return listItem;
+    } else if (message.emetteur === chatSelect){
+      if(message.destinataire === pseudoActuel){
+        let listItem = document.createElement('li');
+        listItem.classList.add("recu")
+        listItem.innerHTML = `${message.content}`;
+        return listItem;
+      }
+    }
+  });
+  messages.forEach((item) => {
+    if(item !== undefined){
+      messagelist.appendChild(item);
+    }
+  });
+
+
+})
+
+function selectionChat(pseudo) {
+  chatSelect = pseudo;
+  socket.emit('syncMessage', chatSelect)
+}
+
+/*
+
+    Chat Discord
+
+*/
+
+function mes() {
+  var input = document.getElementById('input')
+  let message = input.value;
+  let messageObject = {
+    emetteur: pseudoActuel,
+    destinataire: chatSelect,
+    date: new Date(),
     content: message
   };
-
   socket.emit('message', messageObject);
-});
+  input.value = '';
+}
+
 
 socket.on('message', (data) => {
-  const listItem = document.createElement('li');
-  if (data.type === 'recu') {
-    listItem.classList.add("recu")
-    listItem.innerHTML = data.content;
-  } else if (data.type === 'envoi') {
+  var messages = document.getElementById('messages')
+  let listItem = document.createElement('li');
+  if (data.emetteur === pseudoActuel) {
     listItem.classList.add("envoi")
     listItem.innerHTML = data.content;
+  } else if (data.destinataire === chatSelect) {
+    listItem.classList.add("recu")
+    listItem.innerHTML = data.content;
   }
-
   messages.appendChild(listItem);
 });
 
--- server.js
+++ server.js
@@ -3,39 +3,90 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const nodemailer = require("nodemailer");
+const bcrypt = require('bcrypt');
 
-// connexion a la BDD pour les utilisateurs
-const uri = "mongodb+srv://AppSecurityPartner:EEpwNzf1LM6R95S0 @cluster0.bguu1an.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+/*
+
+    Mise en place de la connection a la BDD
+
+*/
+const mongoose = require('mongoose');
+const uri = 'mongodb+srv://CyberSite:BCqyr28eVMnm9htN@cluster0.c6zinmu.mongodb.net/SioDiscord';
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log('Connected to MongoDB Atlas')).catch((err) => console.error('Error connecting to MongoDB Atlas', err));
+// creation du schema de la BDD user
+const userSchema = new mongoose.Schema({
+  pseudo: String,
+  email: String,
+  password: String,
+  inscription: Date,
+  lastConnexion: Date,
+});
+const User = mongoose.model('User', userSchema);
+
+// creation du schema de la BDD message
+const messageSchema = new mongoose.Schema({
+  emetteur: String,
+  destinataire: String,
+  date: Date,
+  content: String,
+});
+const Message = mongoose.model('Message', messageSchema);
+
+
 
 const io = new Server(server);
 var path = require("path");
 const { Socket } = require('dgram');
 let PORT = 3030;
 
+/*
 
+  Service permetant l'envoie d'email en utilisant SMTP
+
+*/
 // Créer un transporteur SMTP
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false, // true pour les ports 465, false pour les autres ports
-    auth: {
-        user: "sio2discord123@gmail.com", // adresse email de l'expéditeur
-        pass: "wglxikcujsfeqxtx" // mot de passe de l'expéditeur
-    }
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true pour les ports 465, false pour les autres ports
+  auth: {
+    user: "sio2discord123@gmail.com", // adresse email de l'expéditeur
+    pass: "wglxikcujsfeqxtx" // mot de passe de l'expéditeur
+  }
 });
 
+/*
+
+    Fonction pour generer le code d'authenfication
+
+*/
+// generation d'un code aleatoire pour verifier l'email
+function generateRandomCode() {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  const length = 6;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+
+
+/*
+
+    Fonction pour envoyer un email
+
+*/
 
 // Vérifier si l'email existe
-async function checkEmailExistence(email) {
+async function checkEmailExistence(email, code) {
   // Définir le destinataire de l'email
   const mailOptions = {
-      from: "sio2discord123@gmail.com",
-      to: email,
-      subject: "Vérification de l'adresse email",
-      text: "Veuillez ignorer ce message s'il vous plaît."
+    from: "sio2discord123@gmail.com",
+    to: email,
+    subject: "Vérification de l'adresse email",
+    text: `Voici votre code d'authenfication \n ${code}`
   };
 
   try {
@@ -54,92 +105,218 @@ server.listen(PORT, () => {
   console.log("Serveur démarré sur le port :" + PORT);
 });
 
+/*
 
+    Routage
+
+*/
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, '/views/index.html'));
 
 });
-
 // css
 app.get("/style", (req, res) => {
   res.sendFile(path.join(__dirname, '/public/stylesheets/style.css'));
 });
-
 app.get("/fontawesome", (req, res) => {
   res.sendFile(path.join(__dirname, '/public/stylesheets/fontawesome-free-6.3.0-web/css/fontawesome.css'));
 });
-
 app.get("/reset.min", (req, res) => {
   res.sendFile(path.join(__dirname, '/public/stylesheets/reset.min.css'));
 });
-
 // script
 app.get("/script", (req, res) => {
   res.sendFile(path.join(__dirname, '/public/javascripts/script.js'));
 });
-
 app.get("/tailwindcss", (req, res) => {
   res.sendFile(path.join(__dirname, '/public/javascripts/tailwindcss.js'));
 });
 
+// JSON des utilisateur connecter pour les envoyer plus tard permetant de savoir qui est en ligne
+let listUsers = {};
 
 
-let users = {};
+/*
+
+    Gestion des envoies et des recus entre le client et le server
+
+*/
 
 io.on('connection', (socket) => {
+  // permet de cree la date d'aujourd'hui
+  var today = new Date();
 
+  /*
+
+    Verification si l'email est deja presente dans la BDD et envoi d'un email contenant un code d'authenfication
+
+  */
   socket.on('sign', (sign) => {
-    // Utilisation de la fonction pour vérifier si l'email existe
-    checkEmailExistence(sign.email).then(exists => {
-      if (exists) {
-        console.log("L'email existe.");
-      } else {
-        console.log("L'email n'existe pas.");
-      }
-    });
+    // creation du code aleatoire qui permetera de verifier si le code envoyer par mail est bon
+    const code = generateRandomCode();
+    // permet de trouver si l'email existe ou pas
+    User.find({ email: sign.email })
+      .then((users) => {
+        if (users.length > 0) {
+          socket.emit('verifEmail', false);
+          // si elle existe retourne true
+        } else {
+          socket.emit('verifEmail', true);
+          // Utilisation de la fonction pour vérifier si l'email existe et permet d'envoyer un email contenant le code
+          checkEmailExistence(sign.email, code).then(exists => {
+            if (exists) {
+              console.log("L'email existe.");
+              socket.emit('verif', code);
+            } else {
+              console.log("L'email n'existe pas.");
+            }
+          });
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   })
 
-  console.log('User connected: ' + socket.id);
+  /*
 
-  // Demander le prénom de l'utilisateur
-  socket.emit('request name');
+    Verification si l'email est deja presente dans la BDD et envoi d'un email contenant un code d'authenfication
 
-  socket.on('name', (name) => {
+  */
+
+  socket.on('register', (user) => {
+    // recupere le password rentrer par l'utilisateur
+    const plainPassword = user.password;
+    // obtient un salage pour hash le code apres
+    const saltRounds = 10;
+    // permet de hasher le password avant de le rentrer dans la BDD
+    bcrypt.hash(plainPassword, saltRounds, async function (err, hash) {
+      if (err) {
+        console.log(err)
+      }
+      // connection a la BDD en entrant les donne du nouvelle utilisateur
+      try {
+        const updatedUser = await User.findOneAndUpdate(
+          {
+            pseudo: user.pseudo,
+            email: user.email,
+            password: hash
+          },
+          {
+            $setOnInsert: { inscription: today },
+            $set: { lastConnexion: today }
+          },
+          {
+            upsert: true,
+            new: true
+          }
+        );
+      } catch (error) {
+        console.error(error);
+      }
+    })
     // Stocker l'ID et le prénom de l'utilisateur
-    users[socket.id] = { id: socket.id, name: name };
-
+    listUsers[socket.id] = { id: socket.id, name: user.pseudo };
     // Envoyer la liste des utilisateurs connectés à tous les clients
-    io.emit('connected users', Object.values(users));
+    io.emit('connected users', Object.values(listUsers));
   });
+  /*
 
+     Verification si l'email et le mot de passe sont bien valide et envoi le resultat au client
+
+  */
+
+  socket.on('login', login => {
+    User.find({ email: login.email }).then((users) => {
+      bcrypt.compare(login.password, users[0].password).then((result) => {
+        if (result) {
+          log = {
+            exists: true,
+            pseudo: users[0].pseudo
+          }
+          socket.emit('login', log)
+          // Stocker l'ID et le prénom de l'utilisateur
+          listUsers[socket.id] = { id: socket.id, name: users[0].pseudo };
+          // Envoyer la liste des utilisateurs connectés à tous les clients
+          io.emit('connected users', Object.values(listUsers));
+        } else {
+          log = {
+            exists: false,
+          }
+          socket.emit('login', log)
+        }
+      })
+        .catch((error) => {
+          console.error(error);
+          log = {
+            exists: false,
+          }
+          socket.emit('login', log)
+        });
+    })
+      .catch((err) => {
+        console.error(err);
+        log = {
+          exists: false,
+        }
+        socket.emit('login', log)
+      });
+  })
+
+  /*
+
+    Service d'envoie de message
+
+  */
+  socket.on('syncMessage', pseudo => {
+    Message.find({
+      $or: [
+        { emetteur: pseudo },
+        { destinataire: pseudo }
+      ]
+    }).then((chat) => {
+      console.log(chat)
+      socket.emit('sync', chat)
+    }).catch((err) => {
+      console.error(err);
+    });
+  })
   socket.on('message', (data) => {
-    console.log(`Message reçu : ${data}`);
+    console.log(`Message reçu : ${data.content}`);
+    /*
 
-    const message = {
-      type: 'recu',
+     implemantation du message dans la bdd message
+
+     */
+    // creation d'un objet message
+    const newMessage = new Message({
+      emetteur: data.emetteur,
+      destinataire: data.destinataire,
+      date: data.date,
       content: data.content
-    };
+    });
 
+    // Enregistrer l'utilisateur dans la base de données
+    newMessage.save().then(() => {
+      console.log("message saved successfully");
+    }).catch((error) => {
+      console.log("Error saving user: ", error);
+    });
     // Envoyer le message reçu à tous les autres clients connectés
-    socket.broadcast.emit('message', message);
-
+    socket.broadcast.emit('message', data);
     // Envoyer le message au client qui l'a envoyé
-    const response = {
-      type: 'envoi',
-      content: data.content
-    };
-    socket.emit('message', response);
+    socket.emit('message', data);
   });
+
 
   // Gérer la perte de connexion du client
   socket.on('disconnect', () => {
     console.log('utilisateur deconnecte: ' + socket.id);
 
     // Retirer l'utilisateur déconnecté de la liste
-    delete users[socket.id];
+    delete listUsers[socket.id];
 
     // Envoyer la liste des utilisateurs connectés à tous les clients
-    io.emit('connected users', Object.values(users));
-
+    io.emit('connected users', Object.values(listUsers));
   });
 });
